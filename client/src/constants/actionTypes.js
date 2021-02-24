0e74a11fca2db5210845679c91765ec4b911f546
Novohatniy, Anatoliy <anatoliy.novohatniy@hsrw.org>
2021-02-24 01:01:16 +0100

    auth with google and auth with jwt

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,5 +1,10 @@
+// POSTS
 export const FETCH_ALL = 'FETCH_ALL'
 export const CREATE = 'CREATE'
 export const UPDATE = 'UPDATE'
 export const DELETE = 'DELETE'
 export const LIKE = 'LIKE'
+
+// AUTHENTICATION
+export const AUTH = 'AUTH'
+export const LOGOUT = 'LOGOUT'
